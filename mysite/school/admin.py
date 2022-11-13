878899117725d0218f80006ccc71fdd3e3575608
Daniel <sisilickmyass@gmail.com>
2022-11-13 19:31:30 +0100

    new models

--- mysite/school/admin.py
+++ mysite/school/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teacher, Studnet_Class, Student, Mark
+
+admin.site.register(Teacher)
+admin.site.register(Studnet_Class)
+admin.site.register(Student)
+admin.site.register(Mark)
--- mysite/school/models.py
+++ mysite/school/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# Create your models here.
+TEST_CHOICES = [
+    ('quiz', 'quiz'),
+    ('exam', 'exam'),
+    ('text', 'test'),
+    ('oral', 'oral')
+]
+
+SCHOOL_SUBJECTS = [
+    ('english', 'english'),
+    ('history', 'history'),
+    ('PE', 'PE'),
+    ('math', 'math'),
+    ('economy', 'economy'),
+    ('geography', 'geography'),
+    ('psychics', 'psychics'),
+]
+
+class Teacher(models.Model):
+    first_name = models.CharField(blank=False)
+    surname = models.CharField(blank=False)
+
+    def __str__(self):
+        return self.first_name + self.surname
+
+    class Meta:
+        verbose_name = 'Teacher'
+        verbose_name_plural = 'Teachers'
+
+
+class Studnet_Class(models.Model):
+    name = models.CharField(blank=False, unique=True)
+    special_class = models.BooleanField(default=False)
+    educator = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Student Class'
+        verbose_name_plural = 'Student Classes'
+
+class Student(models.Model):
+    first_name = models.CharField(blank=False)
+    surname = models.CharField(blank=False)
+    disability = models.BooleanField(default=False)
+    age = models.IntegerField(default=6, blank=False)
+    school_class = models.ForeignKey(Studnet_Class, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.first_name + self.surname
+
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+
+class Mark(models.Model):
+    mark = models.IntegerField(default=5, blank=False)
+    mark_type = models.CharField(choices=TEST_CHOICES, blank=False)
+    weight = models.IntegerField(default=2)
+    subject = models.CharField(choices=SCHOOL_SUBJECTS)
+    pub_date = models.DateTimeField(auto_now=True)
+    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    teachar = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.mark + 'from' + self.subject
+        
+    class Meta:
+        verbose_name = 'Mark'
+        verbose_name_plural = 'Marks'
+
+
+
+
+
